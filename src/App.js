0abb490972d6c2824116c18eb90631015205c296
gustavvopenna <gustavo.pema@gmail.com>
2020-06-30 21:25:48 -0500

    [class 68] Add routing boilerplate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import './App.css'
 
+import { Switch, Route } from "react-router-dom";
+
 import HomePage from './pages/homepage/homepage.component'
 
-const App = () => <HomePage />
+const HatsPage = () => (
+  <div>
+    <h1>Hats pages</h1>
+  </div>
+)
+
+const App = () => (
+  <Switch>
+    <Route exact path='/' component={HomePage} />
+    <Route exact path='/hats' component={HatsPage} />
+  </Switch>
+)
 
 export default App
